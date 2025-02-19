962c0658c6c43420a584cb1ad7751b32dbfd27af
YFshadaow <renzd2001@gmail.com>
2025-02-19 20:27:16 +0800

    Update to 2.0 to adapt to GameUtils refactor

--- src/main/java/fun/kaituo/gametemplate/GameTemplate.java
+++ src/main/java/fun/kaituo/gametemplate/GameTemplate.java
@@ -1,46 +1,39 @@
 package fun.kaituo.gametemplate;
 
+import fun.kaituo.gametemplate.state.TemplateState;
 import fun.kaituo.gameutils.GameUtils;
-import fun.kaituo.gameutils.event.PlayerChangeGameEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import fun.kaituo.gameutils.game.Game;
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+@SuppressWarnings("unused")
+public class GameTemplate extends Game {
+    @Override
+    public void addPlayer(Player player) {
 
-
-public class GameTemplate extends JavaPlugin implements Listener {
-    private GameUtils gameUtils;
-
-    List<Player> players;
-
-    public static GameTemplateGame getGameInstance() {
-        return GameTemplateGame.getInstance();
     }
 
+    @Override
+    public void removePlayer(Player player) {
 
+    }
 
+    @Override
+    public void forceStop() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
 
     public void onEnable() {
-        gameUtils = (GameUtils) Bukkit.getPluginManager().getPlugin("GameUtils");
-        players = new ArrayList<>();
-        Bukkit.getPluginManager().registerEvents(this, this);
-        gameUtils.registerGame(getGameInstance());
+        super.onEnable();
+        setState(new TemplateState());
+        updateExtraInfo("§f示例游戏", GameUtils.inst().getMainWorld().getSpawnLocation());
     }
 
     public void onDisable() {
-        Bukkit.getScheduler().cancelTasks(this);
-        HandlerList.unregisterAll((Plugin) this);
-        for (Player p: Bukkit.getOnlinePlayers()) {
-            if (gameUtils.getPlayerGame(p) == getGameInstance()) {
-                Bukkit.dispatchCommand(p, "join Lobby");
-            }
-        }
-        gameUtils.unregisterGame(getGameInstance());
+        super.onDisable();
     }
 }
